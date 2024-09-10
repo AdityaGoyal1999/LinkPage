19f5bb9eb7af3ff574f8fbf4eeb37224dc13316c
Aditya Goyal <adityagoyaldoon@gmail.com>
2024-09-10 16:12:01 -0400

    Key up down functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import './App.css'
 import ProfilePic from './assets/avatar-jessica.jpeg'
+import { useState, useRef, useEffect } from 'react';
 
 const buttonTexts = ['Github', 'Frontend Mentor', 'Linkedin', 'Twitter', 'Instagram']
 
 function App() {
+
+  const [focusedIndex, setFocusedIndex] = useState(0); // current focused button index
+  const buttonRefs = useRef([])
+
+  useEffect(() => {
+    if (buttonRefs.current[focusedIndex]) {
+      buttonRefs.current[focusedIndex].focus();
+    }
+  }, [focusedIndex])
+
+  const handleKeyDown = (event) => {
+    switch (event.key){
+      case 'ArrowDown':
+        setFocusedIndex((prevIndex) => (prevIndex + 1) % buttonTexts.length);
+        break;
+      case 'ArrowUp':
+        setFocusedIndex((prevIndex) => (prevIndex - 1 + buttonTexts.length) % buttonTexts.length);
+        break;
+      case 'Enter':
+        console.log(`${buttonTexts[focusedIndex]} button activated`);
+        break;
+      default:
+        break;
+    }
+  }
 
   return (
     <div className="bg-grey-900 w-screen h-screen flex justify-center items-center">
@@ -16,9 +42,16 @@ function App() {
 
         <div className="w-full">
         {
-          buttonTexts.map((text) => {
+          buttonTexts.map((text, index) => {
             return (
-            <div key={text} className="bg-grey-700 p-3 m-4 rounded-lg hover:bg-green text-customWhite hover:text-grey-700 transition-transform duration-200 ease-in-out active:scale-95">
+            <div 
+              key={index} 
+              ref={(el) => (buttonRefs.current[index] = el)} 
+              className="bg-grey-700 p-3 m-4 rounded-lg hover:bg-green text-customWhite hover:text-grey-700 focus:text-grey-700 focus:bg-green focus:outline-none transition-transform duration-200 ease-in-out active:scale-95"
+              onClick={() => console.log(`${buttonTexts[index]} pressed`)}
+              tabIndex={0}
+              onKeyDown={handleKeyDown}
+              >
                 <p className="text-center">{text}</p>
               </div>
             )
